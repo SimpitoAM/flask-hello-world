52c8828f8291313a3343663a9bc14a00ad6f371a
MWILA AARON SIMPITO <amsimpito@gmail.com>
2016-12-11 01:14:42 +0200

    adding update app.py file

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-# ----- Flask Hello World -------#
-
-# import the Flask package
 
 from flask import Flask
 
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
 
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World! Does this work ?"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
